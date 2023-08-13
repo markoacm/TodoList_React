d10eae41433e854f7bfdf0d633f2e50a770348d0
Marco_acm <71322098+markoacm@users.noreply.github.com>
2023-08-13 22:23:23 +0000

    small change in design

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import InputTask from "./input";
 
-
 //create your first component
 const Home = () => {
-    return (
-        
-            <div className="container">
-                <h1 className="display-1">Todos</h1>
-                <InputTask/>
-            </div>
-            
-        
-            
-    
-            
-    );
+  return (
+    <div className="container">
+      <h1 className="display-1">Todos</h1>
+      <InputTask />
+    </div>
+  );
 };
 
 export default Home;
--- src/js/component/input.js
+++ src/js/component/input.js
@@ -1,45 +1,52 @@
 import React, { useState } from "react";
 
 const InputTask = () => {
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState([]);
 
-    const [input, setInput] = useState("");
-    const [task, setTask] = useState([]);
-    
-    const handleChange = (event) => {
-        setInput(event.target.value);
-    };
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            if (input !== "")
-            setTask([input].concat(task));
-            setInput("");
-        }
-    } ;
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (input !== "") setTask([input].concat(task));
+      setInput("");
+    }
+  };
 
-    const handleClick = (index) => {
-        setTask(task.filter((t, currentIndex) => currentIndex !== index));
-    };
+  const handleClick = (index) => {
+    setTask(task.filter((t, currentIndex) => currentIndex !== index));
+  };
 
+  return (
+    <div className="task-input">
+      <input
+        type="text"
+        id="new-task"
+        name="new-task"
+        placeholder="What needs to be done?"
+        value={input}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <ul>
+        {task.map((item, index) => (
+          <li>
+            {item}
+            <button
+              className="btn"
+              type="button"
+              onClick={() => handleClick(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="items">{task.length} items</div>
+    </div>
+  );
+};
 
-    return (
-
-            <div className="task-input">
-                 <input type="text" id="new-task" name="new-task" placeholder="What needs to be done?"
-                 value={input} onChange={handleChange} onKeyDown={handleKeyDown}/>
-                    <ul>
-                        {task.map((item,index) => ( 
-                        <li >{item}<button className="btn" type="button" onClick={() => handleClick(index)}>X</button>
-                        </li>
-                        ))}
-                    
-                    </ul>
-                <div className="items">{task.length} items</div>
-            </div>
-        
-    )
-}
-
-export default InputTask
-
-    
+export default InputTask;
